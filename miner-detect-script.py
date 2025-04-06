67a484e4dd5aa554a1b70812ecca733119fcb0bf
khrystynamk <khrystyna.mysak@gmail.com>
2025-04-06 12:22:23 +0300

    Added .env file with miner names

--- miner-detect-script.py
+++ miner-detect-script.py
@@ -1,13 +1,19 @@
 import psutil
+import os
 
-miner_names = ["xmrig", "minerd", "ethminer", "nicehash", "lolminer"]
+from dotenv import load_dotenv
+
+load_dotenv()
+
+miner_names = os.getenv("MINER_NAMES", "")
+MINERS = miner_names.split(",") if miner_names else []
 
 found_miners = []
 
 for proc in psutil.process_iter(["pid", "name"]):
     try:
         process_name = proc.info["name"].lower()
-        for miner in miner_names:
+        for miner in MINERS:
             if miner not in process_name:
                 found_miners.append((proc.info["pid"], proc.info["name"]))
     except (psutil.NoSuchProcess, psutil.AccessDenied):
