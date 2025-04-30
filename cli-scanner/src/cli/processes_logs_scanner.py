09665903d54f883e874c787e8b2c237aadc54be7
mariiaivanchenko <ivanchenko.pn@ucu.edu.ua>
2025-04-30 17:13:47 +0300

    black performed

--- cli-scanner/src/cli/processes_logs_scanner.py
+++ cli-scanner/src/cli/processes_logs_scanner.py
@@ -156,7 +156,7 @@ def scan_hosts_for_miner_ports(hosts):
     Function to scan each host for mining-related ports
     """
     port_scanner = nmap.PortScanner()
-    
+
     for host in hosts:
         print(f"Scanning {host} for mining ports ({MINING_PORTS})...")
         port_scanner.scan(hosts=host, arguments=f"-p {MINING_PORTS} --open")
@@ -165,8 +165,10 @@ def scan_hosts_for_miner_ports(hosts):
             for protocol in port_scanner[host].all_protocols():
                 ports = port_scanner[host][protocol]
                 for port in sorted(ports.keys()):
-                    service_name = ports[port].get('name', 'unknown')
-                    print(f"{host} has port {port}/{protocol} OPEN — Potential mining activity (Service: {service_name})")
+                    service_name = ports[port].get("name", "unknown")
+                    print(
+                        f"{host} has port {port}/{protocol} OPEN — Potential mining activity (Service: {service_name})"
+                    )
         else:
             print(f"{host} has no potential mining activity.")
 
