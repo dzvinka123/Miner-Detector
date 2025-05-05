856e4d1661743c6257065951e1775f4314b12d8b
khrystynamk <khrystyna.mysak@gmail.com>
2025-05-05 12:50:55 +0300

    fix: fixed path

--- tests/unittests/core/test_utils.py
+++ tests/unittests/core/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 import pytest
-from core.util import parse_time_threshold, send_report_to_server
+from src.core.util import parse_time_threshold, send_report_to_server
 from unittest.mock import patch, Mock
 
 
